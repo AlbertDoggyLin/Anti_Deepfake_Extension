3d110c116737a2c54c4d43646fd7685f55686344
AlbertDoggyLin <albertlin2468@gmail.com>
2021-12-11 14:09:21 +0800

    encounter error---failed to fetch model

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-let color='red'
-
-
 chrome.runtime.onInstalled.addListener(()=>{
-    chrome.storage.sync.set({color});
-    console.log('Default background color set to %cgreen', `color: ${color}`);
+    console.log('Welcome')
 });
--- popup.js
+++ popup.js
@@ -1,29 +1,37 @@
-let changeColor = document.getElementById("changeColor")
-
-
-chrome.storage.sync.get("color", ({color}) => {
-    changeColor.style.backgroundColor = color;
-});
-
-
-function drawImgs(){
-    chrome.storage.sync.get("color", ({color}) => {
-        var images = document.getElementsByTagName('img'); 
-        var srcList = [];
-        for(var i = 0; i < images.length; i++) {
-            srcList.push(images[i].src);
-            images[i].style.backgroundColor=color;
-            images[i].style.border='red 5px solid';
-            images[i].style.boxSizing = "border-box";
-        }
+let session;
+async function prework(){
+    let startBut = document.getElementById("StartButton")
+    startBut.style.backgroundColor = 'green';
+    session = await ort.InferenceSession.create('./AntiDeepfake_mobilenetv3_small.onnx');
+    startBut.addEventListener("click", async()=>{
+        let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: main
+        });
     });
 }
+prework();
+async function main(){
+    try{
+        let color={fakeColor: 'red', realColor: 'green'}
+        console.log(`session=${session}`)
+        function isFake(img){
+            return false;
+        };
+        let images = document.getElementsByTagName('img');
+        for(let i = 0; i < images.length; i++) {
+            images[i].style.boxSizing = "border-box";
+            if(isFake(images[i])){
+                images[i].style.border=`${color.fakeColor} 5px solid`;
+            }
+            else{
+                images[i].style.border=`${color.realColor} 5px solid`;
+            }
+        }
+    }
+    catch(e){
+        console.log(e);
+    }
+}
 
-
-changeColor.addEventListener("click", async()=>{
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: drawImgs
-    });
-});
